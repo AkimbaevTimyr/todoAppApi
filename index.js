5bc04a3e5b206b8742807db756936a7cdb2f6783
AkimbaevTimyr <60063664+AkimbaevTimyr@users.noreply.github.com>
2022-06-05 17:13:00 +0300

    1

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 require('dotenv').config()
 const express = require('express')
-const router = require('./routes/index') //импортируем роуты
+const router = require('./routes/index') 
 const PORT = process.env.PORT || 5000
 const cors = require('cors')
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use('/api', router) //инициализируем роуты
+app.use('/api', router) 
 
 
 app.listen(PORT, ()=> console.log('SERVER IS RUNNIG'))
 
 
-module.exports = (req, res) => {
-    const { name = 'World' } = req.query;
-    res.send(`Hello ${name}!`);
-  };
-  
