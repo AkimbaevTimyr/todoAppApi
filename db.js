82d00cee346c447b3da22f2004c4d51da4d057e3
AkimbaevTimyr <60063664+AkimbaevTimyr@users.noreply.github.com>
2022-06-06 12:36:58 +0300

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const Pool = require('pg').Pool
 const pool = new Pool({
     user: 'oohvmwzwanjqcj',
     host: "ec2-3-211-221-185.compute-1.amazonaws.com",
-    database: "tododb",
+    database: "deng5ct6rpmd2l",
     password: 'bbbe731bd01f0bd5bf23732b17baf51d5d0aae57a12b6ff00c9309ee7f1dbfb1',
     port: 5432,
 })
