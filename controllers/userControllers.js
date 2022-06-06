25ad55d68a0c929e7254260a1e8fa44f43149512
AkimbaevTimyr <60063664+AkimbaevTimyr@users.noreply.github.com>
2022-06-06 12:18:00 +0300

    Update userControllers.js

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -12,8 +12,12 @@ const generateJwt = (id, email, hashPassword) =>{
 }
 class UserController{
     async registration(req, res, next){
-        try{
-            const {id, email, password} = req.body
+        // try{
+            
+        // }catch(e){
+        //     next(ApiError.badRequest(e.message))
+        // }
+        const {id, email, password} = req.body
             if(!email || !password || !id){
                 next(ApiError.badRequest('Некорректный email или password'))
             }
@@ -25,27 +29,25 @@ class UserController{
             const newUser = await db.query(`INSERT INTO person (id, email, password) values ($1, $2, $3) RETURNING *`, [id, email, hashPassword])
             const token = generateJwt(id, email, password)
             return res.json({token })
-        }catch(e){
-            next(ApiError.badRequest(e.message))
-        }
     }
     // сделать вывод не верного пароля и тд
     async login(req, res, next){
-        try{    
-            const {email, password} = req.body
-            const user = await db.query(`select * from person where email = $1`,[email])
-            let id = user.rows[0].id
-            if(!user){
-               return dialog.info('Пользователь не найден')
-            }
-            if(user.rows[0].password != password){
-               next(ApiError.badRequest('Не верный пароль'))
-            }
-            const token = generateJwt(user.id, user.email)
-            return res.json([token, id])
-        }catch(e){
-            next(ApiError.badRequest(e.message))
+        // try{    
+            
+        // }catch(e){
+        //     next(ApiError.badRequest(e.message))
+        // }
+        const {email, password} = req.body
+        const user = await db.query(`select * from person where email = $1`,[email])
+        let id = user.rows[0].id
+        if(!user){
+           return dialog.info('Пользователь не найден')
         }
+        if(user.rows[0].password != password){
+           next(ApiError.badRequest('Не верный пароль'))
+        }
+        const token = generateJwt(user.id, user.email)
+        return res.json([token, id])
     }
     async check(req, res, next){
         const token = generateJwt(req.user.id, req.user.email, req.user.password)
