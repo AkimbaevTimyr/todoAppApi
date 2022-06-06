1cf3f14b56d9bca345d1ee9477c50cae6594211d
AkimbaevTimyr <60063664+AkimbaevTimyr@users.noreply.github.com>
2022-06-06 13:39:35 +0300

    Update db.js

--- db.js
+++ db.js
@@ -6,7 +6,12 @@ const pool = new Pool({
     database: "deng5ct6rpmd2l",
     password: 'bbbe731bd01f0bd5bf23732b17baf51d5d0aae57a12b6ff00c9309ee7f1dbfb1',
     port: 5432,
-    ssl: false,
+    ssl: {
+        rejectUnauthorized : false,
+        ca   : fs.readFileSync("server-ca.pem").toString(),
+        key  : fs.readFileSync("client-key.pem").toString(),
+        cert : fs.readFileSync("client-cert.pem").toString(),
+  }
 })
 
 
