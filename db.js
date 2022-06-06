ceb8cd35f74afb007e50c160bb1e5a5b25af17f5
AkimbaevTimyr <60063664+AkimbaevTimyr@users.noreply.github.com>
2022-06-06 11:10:48 +0300

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool
 
 const pool = new Pool({
     user: 'postgres',
-    host: "https://node-postgres-cloud.herokuapp.com/",
+    host: "107.22.57.98:443",
     database: "tododb",
     password: '198305',
     port: 5432,
