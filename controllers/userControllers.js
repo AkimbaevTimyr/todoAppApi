97e120459782698e71a887edd65a7c622b48ff01
AkimbaevTimyr <60063664+AkimbaevTimyr@users.noreply.github.com>
2022-06-18 13:54:20 +0300

    commit

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -3,51 +3,44 @@ const ApiError = require('../error/error')
 const jwt = require('jsonwebtoken')
 const SECRET_KEY = process.env.SECRET_KEY
 
-const generateJwt = (id, email, password) =>{
+const generateJwt = (id, email, password) => {
     return jwt.sign(
-        {id, email, password},
+        { id, email, password },
         SECRET_KEY,
-        {expiresIn: '1h'}
+        { expiresIn: '1h' }
     )
 }
-class UserController{
-    async registration(req, res, next){
-        try{
-            const {id, email, password} = req.body
-            if(!email || !password || !id){
+class UserController {
+    async registration(req, res, next) {
+        try {
+            const { id, email, password } = req.body
+            if (!email || !password || !id) {
                 next(ApiError.badRequest('Некорректный email или password'))
             }
             const candidate = await db.query(`select * from person where email = $1`, [email])
             let person = candidate.rows
-            if(person === true){
+            if (person === true) {
                 return next(ApiError.badRequest('Пользователь с таким email уже существует'))
             }
             const newUser = await db.query(`INSERT INTO person (id, email, password) values ($1, $2, $3) RETURNING *`, [id, email, password])
             const token = generateJwt(id, email, password)
-            return res.json({token })
-        }catch(e){
+            return res.json({ token })
+        } catch (e) {
             next(ApiError.badRequest(e.message))
         }
     }
     // сделать вывод не верного пароля и тд
-    async login(req, res, next){
-        try{    
-            const {email, password} = req.body
-            const user = await db.query(`select * from person where email = $1`,[email])
-            let id = user.rows[0].id
-            if(!user){
-               return dialog.info('Пользователь не найден')
-            }
-            if(user.rows[0].password != password){
-               next(ApiError.badRequest('Не верный пароль'))
-            }
-            const token = generateJwt(user.id, user.email)
-            return res.json([token, id])
-        }catch(e){
-            next(ApiError.badRequest(e.message))
+    async login(req, res, next) {
+        const { email, password } = req.body
+        const user = await db.query(`select * from person where email = $1`, [email]);
+        let id = user.rows[0].id;
+        if (user.rows[0].password != password) {
+            return res.json(["Не верный пароль"])
         }
+        const token = generateJwt(user.id, user.email)
+        return res.json([token, id])
     }
-    async check(req, res, next){
+    async check(req, res, next) {
         const token = generateJwt(req.user.id, req.user.email, req.user.password)
         return res.json(token)
     }
--- index.js
+++ index.js
@@ -8,9 +8,6 @@ app.use(cors())
 app.use(express.json())
 app.use('/api', router) 
 
-const routes = app._router.stack.filter(r => r.route).map(r => r.route.path) 
-console.log(routes)
-
 app.listen(PORT, ()=> console.log('SERVER IS RUNNIG'))
 
 
